4fff36c3d935d2d13f7b53efabe82a36c064618d
Sergio Peña <sergiop79@gmail.com>
2013-06-02 11:45:33 +0200

    app.js -ri kpmentarioak jarrita

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+// express liburutegiari deitzen diogu web zerbitzaria sortzeko
 var express = require('express');
 var app = express();
 
-
+// aplikazioa konfigurazioa
 app.configure(function() {
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	app.use(express.static(__dirname + '/public'));
+	app.set('views', __dirname + '/views');		// bistak gordetzeko karpeta definitzen dugu
+	app.set('view engine', 'jade');			// bistarako, jade txantiloi motorra erabiliko da
+	app.use(express.static(__dirname + '/public'));	// karpeta estatiko bat definitzen dugu css irudi eta js-ak gordetzeko
 });
 
-
+// errora heltzen diren eskaera guztiei bista nagusia erakutsiko zaie
 app.get('/', function(req,res) {
 	res.render('layout',{
 		title: 'TxioMapa',
@@ -18,7 +19,7 @@ app.get('/', function(req,res) {
 
 
 
-
+// web aplikazioa 3000 portua entzuten egongo da
 app.listen(3000);
 
 console.log('Aplikazioa martxan http://locahost:3000 helbidean.');
